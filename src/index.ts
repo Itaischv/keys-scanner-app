7365bf633322d633df5a2f0eadef7e3521888e53
Itai Schvartzbard <itai@practitest.com>
2025-10-23 18:25:53 +0300

    Accepting parameters to perform scan on org/repo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import dotenv from 'dotenv';
-import { getCommits } from "./services/githubService";
+import { scanRepo } from "./scanner";
 
 dotenv.config();
 
 console.log("GitHub Token prefix:", process.env.GH_TOKEN?.slice(0, 5) || "missing");
 
-const commits = await getCommits("octocat", "Hello-World", 1, 5);
-console.debug("Commits:", commits)
+(async () => {
+    const repoArg = process.argv[2];
+    if (!repoArg) {
+        console.error("Usage: npm run scan owner/repo");
+        process.exit(1);
+    }
+
+    const [owner, repo] = repoArg.split("/");
+    await scanRepo(owner, repo);
+})();
